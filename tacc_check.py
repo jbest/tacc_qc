9e4ba657d9d2366d9be30b69b56696a3c5c0e94d
Jason Best <jbest@brit.org>
2020-10-10 17:09:36 -0500

    working with config file;

--- tacc_check.py
+++ tacc_check.py
@@ -6,7 +6,7 @@ import json
 import re
 
 # File extensions that are scanned
-FILE_TYPES = ['.jpg', '.jpeg', '.JPG', '.JPEG']
+#FILE_TYPES = ['.jpg', '.jpeg', '.JPG', '.JPEG']
 
 def show_tree(path=None):
     print('in show_tree:', path)
@@ -15,14 +15,42 @@ def show_tree(path=None):
 
 def walk(path=None):
     scan_start_time = datetime.now()
+    web_jpg_p = re.compile(web_jpg_regex)
+    web_jpg_med_p = re.compile(web_jpg_med_regex)
+    web_jpg_thumb_p = re.compile(web_jpg_thumb_regex)
+    print(web_jpg_thumb_regex)
+
     for root, dirs, files in os.walk(path):
 
         for file in files:
             file_path = Path(root).joinpath(file)
             file_name = file_path.name
-            file_ext = file_path.suffix
+            #file_ext = file_path.suffix
+            if web_jpg_p.match(file_name):
+                # full sized image
+                m = web_jpg_p.match(file_name)
+                #print(m.groupdict())
+                catalog_number = m['catalog_number']
+                if catalog_number not in inventory:
+                    inventory[catalog_number] = {}
+                inventory[catalog_number]['web_jpg'] = file_name
+                print('matches web:', file_name)
+            if web_jpg_thumb_p.match(file_name):
+                print('matches thumb:', file_name)
+                m = web_jpg_thumb_p.match(file_name)
+                catalog_number = m['catalog_number']
+                if catalog_number not in inventory:
+                    inventory[catalog_number] = {}
+                inventory[catalog_number]['web_jpg_thumb'] = file_name
+            if web_jpg_med_p.match(file_name):
+                if catalog_number not in inventory:
+                    inventory[catalog_number] = {}
+                inventory[catalog_number]['web_jpg_med'] = file_name
+                print('matches med:', file_name)
+            """
             if file_ext in FILE_TYPES:
                 print(file_name)
+            """
             #file_path = Path(file).resolve()
             #print('file:',file, file_path)
 
@@ -43,12 +71,14 @@ with open(config_file) as f:
 collection = config.get('collection', None)
 collection_prefix = collection.get('prefix', None)
 files = config.get('files', None)
-#folder_increment = int(files.get('folder_increment', 1000))
-#log_directory_path = Path(files.get('log_directory_path', None))
-#number_pad = int(files.get('number_pad', 7))
 directory_path = Path(files.get('output_base_path', None))
-
+file_types = config.get('file_types', None)
 #directory_path = os.path.realpath(args["directory"])
 
+web_jpg_regex = file_types['web_jpg']['regex']
+web_jpg_med_regex = file_types['web_jpg_med']['regex']
+web_jpg_thumb_regex = file_types['web_jpg_thumb']['regex']
 
+inventory = {} # relevant contents of directory path
 walk(path=directory_path)
+print(inventory)
